43ca3880f74431c19e656d723d7e84cee0686f7d
smabdulmueid97 <73439037+smabdulmueid97@users.noreply.github.com>
2023-01-05 03:06:21 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x1: ");
+        double x1 = input.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = input.nextDouble();
+        System.out.print("Enter x2: ");
+        double x2 = input.nextDouble();
+        System.out.print("Enter x2: ");
+        double y2 = input.nextDouble();
+        System.out.print("Enter x3: ");
+        double x3 = input.nextDouble();
+        System.out.print("Enter y3: ");
+        double y3 = input.nextDouble();
+
+        // Compute the area of a triangle
+        double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+        double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
+        double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+        double s = (side1 + side2 + side3) / 2;
+        double area = Math.pow(s * (s - side1) * (s - side2) * (s - side3), 0.5);
+
+        // Display result
+        System.out.println("The area of the triangle is " + area);
+    }
+}
